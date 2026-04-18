a3681901b3316569c0a341bdc994e844bc8bd8a0
ashwin biju <ashwinbiju011@gmail.com>
2026-04-18 15:31:47 +0530

    for-deploy

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,8 @@ const nextConfig: NextConfig = {
         hostname: "images.unsplash.com",
       },
     ],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
